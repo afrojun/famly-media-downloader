df861a58f7da1eecf84b03eb19e71ac7b5b7909f
Arjun Radhakrishnan <afrojun@users.noreply.github.com>
2021-09-24 07:30:46 +0100

    Split paginated_feed into seperate fetch and process methods

--- script.rb
+++ script.rb
@@ -9,40 +9,48 @@ class Famly
   attr_reader :current_time, :feed_api
 
   BASE_URL = "https://app.famly.co"
-  ACCESS_TOKEN = "ENTER TOKEN HERE!"
 
   def initialize(feed_api: FeedApi.new, current_time: Time.now.utc.to_datetime.iso8601)
     @current_time = current_time
     @feed_api = feed_api
+    @observation_ids = []
   end
 
   def fetch_data
-    pp paginated_feed
+    pp get_observation_ids
   end
 
   private
 
+  def get_observation_ids
+    paginated_feed do |item|
+      @observation_ids.push(
+        item.fetch("embed", {})&.fetch("observationId", nil)
+      )
+    end
+
+    @observation_ids.compact
+  end
+
   def paginated_feed(before: current_time)
     last_item_time = before
-    observation_ids = []
 
     loop do
       feed = feed_api.get(olderThan: last_item_time)
       items = feed["feedItems"]
 
       # break if items.none?
-      break if last_item_time < "2021-08-15T00:00:00+00:00"
+      break if last_item_time < "2021-08-31T00:00:00+00:00"
 
       items.each do |item|
-        observation_ids << item.fetch("embed", {})&.fetch("observationId", nil)
+        yield item
+
         last_item_time = item["createdDate"] if item["createdDate"] < last_item_time
       end
 
       puts last_item_time
       sleep 1
     end
-
-    observation_ids.compact
   end
 
   class FeedApi
@@ -53,7 +61,7 @@ class Famly
       uri.query = URI.encode_www_form(params)
 
       req = Net::HTTP::Get.new(uri)
-      req["x-famly-accesstoken"] = ACCESS_TOKEN
+      req["x-famly-accesstoken"] = ENV.fetch("ACCESS_TOKEN")
       req["content-type"] = "application/json"
 
       res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
