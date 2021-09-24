004553b8cbe737c6acbe0f5b412579ae022d9010
Arjun Radhakrishnan <afrojun@users.noreply.github.com>
2021-09-24 20:42:42 +0100

    Change directory structure to be more normal

---
+++ lib/famly.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "famly/media_downloader"
+
+module Famly
+
+end
---
+++ lib/famly/media_downloader.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative "feed_api"
+
+module Famly
+  class MediaDownloader
+    attr_reader :feed_api
+
+    def initialize(feed_api: FeedApi.new)
+      @feed_api = feed_api
+      @observation_ids = []
+    end
+
+    def fetch_data
+      pp get_observation_ids
+    end
+
+    private
+
+    def get_observation_ids
+      feed_api.paginated_feed do |item|
+        @observation_ids.push(item.dig("embed", "observationId"))
+      end
+
+      @observation_ids.compact
+    end
+  end
+end
